b00fd631b98cbea51c020acf196b033928cc7c28
David Ko <david.holy.ko@gmail.com>
2020-10-15 22:22:50 -0400

    feat(home-page): update homepage with links

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const HomePage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/">Home Page</Link>
+      <Link to="cascade">Cascade Template</Link>
+      <Link to="crip">Crisp Template</Link>
+    </div>
+  );
 };
