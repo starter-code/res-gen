c21ee34df139fd35f0f3fe6356a9e1eb4215e458
PatrickDohn <dohnpatrick1993@gmail.com>
2020-12-08 20:33:51 -0500

    feat(faq-page): initial-setup

---
+++ src/components/pages/FAQPage.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import { NavBar } from 'src/components/main';
+
+export const FAQPage = () => {
+  return (
+    <div className="faq-page">
+      <NavBar />
+      <main className="faq-section">
+        <section className="faq-one">
+          <h1 className="faq-title">Frequently Asked Questions</h1>
+          <div className="faq-content">
+            <h1>Why use us?</h1>
+            <div className="hidden">
+              <p>
+                akfnalgnasdlv dfnsadglsdngs sdgklnsdglsndg
+                sdgklnsdglsndgdsfsmdngssdgs asdfnsdfg sdfsd
+              </p>
+            </div>
+          </div>
+          <div className="faq-content">
+            <h1>Why we made this app?</h1>
+            <div className="hidden">
+              <p>
+                akfnalgnasdlv dfnsadglsdngs sdgklnsdglsndg
+                sdgklnsdglsndgdsfsmdngssdgs asdfnsdfg sdfsd
+              </p>
+            </div>
+          </div>
+          <div className="faq-content">
+            <h1>Who we are?</h1>
+            <div className="hidden">
+              <p>
+                akfnalgnasdlv dfnsadglsdngs sdgklnsdglsndg
+                sdgklnsdglsndgdsfsmdngssdgs asdfnsdfg sdfsd
+              </p>
+            </div>
+          </div>
+          <div className="faq-content">
+            <h1>More Questions?</h1>
+            <div className="hidden">
+              <p>
+                akfnalgnasdlv dfnsadglsdngs sdgklnsdglsndg
+                sdgklnsdglsndgdsfsmdngssdgs asdfnsdfg sdfsd
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="faq-two">
+          <div className="contact-form">
+            <h1>Contact us:</h1>
+            <h3>linkedIn</h3>
+            <h3>linkedIn</h3>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
