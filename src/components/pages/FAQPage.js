06ea49b7758929a28437894c41c763947b76931b
PatrickDohn <dohnpatrick1993@gmail.com>
2020-12-11 14:09:43 -0500

    feat(faq-page): add styling to faq page

--- src/components/pages/FAQPage.js
+++ src/components/pages/FAQPage.js
@@ -1,59 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { NavBar } from 'src/components/main';
+import { NavBar, Footer } from 'src/components/main';
 
 export const FAQPage = () => {
   return (
-    <div className="faq-page">
+    <main className="faq-page">
       <NavBar />
-      <main className="faq-section">
-        <section className="faq-one">
-          <h1 className="faq-title">Frequently Asked Questions</h1>
-          <div className="faq-content">
-            <h1>Why use us?</h1>
-            <div className="hidden">
-              <p>
-                akfnalgnasdlv dfnsadglsdngs sdgklnsdglsndg
-                sdgklnsdglsndgdsfsmdngssdgs asdfnsdfg sdfsd
-              </p>
-            </div>
+      <div className="faq">
+        <section className="faq-section">
+          <h1 className="faq-title">FAQ</h1>
+          <div className="faq-questions">
+            <h1>Who is the target audience of ResGenie?</h1>
+            <h3>
+              Software engineers coming out of a bootcamp who know some CSS and
+              JSON
+            </h3>
           </div>
-          <div className="faq-content">
-            <h1>Why we made this app?</h1>
-            <div className="hidden">
-              <p>
-                akfnalgnasdlv dfnsadglsdngs sdgklnsdglsndg
-                sdgklnsdglsndgdsfsmdngssdgs asdfnsdfg sdfsd
-              </p>
-            </div>
+          <div className="faq-questions">
+            <h1>What technologies are used to build this project</h1>
+            <h3>
+              Its made with React with a custom webpack config. The editor page
+              is made from codemirror to make the editor and React PDF to render
+              the PDF.
+            </h3>
           </div>
-          <div className="faq-content">
-            <h1>Who we are?</h1>
-            <div className="hidden">
-              <p>
-                akfnalgnasdlv dfnsadglsdngs sdgklnsdglsndg
-                sdgklnsdglsndgdsfsmdngssdgs asdfnsdfg sdfsd
-              </p>
-            </div>
+          <div className="faq-questions">
+            <h1>Who is the project for?</h1>
+            <h3>
+              Software engineers coming out of a bootcamp who know some CSS and
+              JSON
+            </h3>
           </div>
-          <div className="faq-content">
-            <h1>More Questions?</h1>
-            <div className="hidden">
-              <p>
-                akfnalgnasdlv dfnsadglsdngs sdgklnsdglsndg
-                sdgklnsdglsndgdsfsmdngssdgs asdfnsdfg sdfsd
-              </p>
-            </div>
+          <div className="faq-questions">
+            <h1>How do i find the code for this project?</h1>
+            <h3>
+              <Link>
+                <h3>Great question click here!</h3>
+              </Link>
+            </h3>
           </div>
-        </section>
-        <section className="faq-two">
-          <div className="contact-form">
-            <h1>Contact us:</h1>
-            <h3>linkedIn</h3>
-            <h3>linkedIn</h3>
+          <div className="faq-questions">
+            <h1>How do i find the code for this project?</h1>
+            <h3>
+              <Link>
+                <h3>Great question click here!</h3>
+              </Link>
+            </h3>
           </div>
         </section>
-      </main>
-    </div>
+      </div>
+      <Footer />
+    </main>
   );
 };
