91356ed7b1ddf9a69542845e26c44815822beb7c
PatrickDohn <dohnpatrick1993@gmail.com>
2020-12-11 18:40:07 -0500

    feat(faq-page): update to styling format and add content

--- src/components/pages/FAQPage.js
+++ src/components/pages/FAQPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import { NavBar, Footer } from 'src/components/main';
 
@@ -11,41 +10,30 @@ export const FAQPage = () => {
         <section className="faq-section">
           <h1 className="faq-title">FAQ</h1>
           <div className="faq-questions">
-            <h1>Who is the target audience of ResGenie?</h1>
-            <h3>
-              Software engineers coming out of a bootcamp who know some CSS and
-              JSON
-            </h3>
-          </div>
-          <div className="faq-questions">
             <h1>What technologies are used to build this project</h1>
             <h3>
-              Its made with React with a custom webpack config. The editor page
-              is made from codemirror to make the editor and React PDF to render
-              the PDF.
+              ResGenie is built with React, with a custom webpack config. The
+              editor page is made from codemirror to make the editor, and React
+              PDF to render the PDF.
             </h3>
           </div>
           <div className="faq-questions">
             <h1>Who is the project for?</h1>
             <h3>
               Software engineers coming out of a bootcamp who know some CSS and
-              JSON
+              JSON.
             </h3>
           </div>
           <div className="faq-questions">
             <h1>How do i find the code for this project?</h1>
             <h3>
-              <Link>
-                <h3>Great question click here!</h3>
-              </Link>
-            </h3>
-          </div>
-          <div className="faq-questions">
-            <h1>How do i find the code for this project?</h1>
-            <h3>
-              <Link>
-                <h3>Great question click here!</h3>
-              </Link>
+              Great question click{' '}
+              <a
+                href="https://github.com/starter-code/res-gen"
+                className="faq-link"
+              >
+                here!
+              </a>
             </h3>
           </div>
         </section>
