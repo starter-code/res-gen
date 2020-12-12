ce8c2d0dbc77e8e5911ccadbd48378d4c75cfb18
David Ko <david.holy.ko@gmail.com>
2020-12-12 12:37:19 -0500

    fix(routes): restore editor lazy route

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,23 +1,22 @@
 import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import {
-  HomePage,
-  ContributorsPage,
-  NotFoundPage,
-  EditorPage,
-  FAQPage,
-} from 'src/pages';
+import { HomePage, ContributorsPage, NotFoundPage, FAQPage } from 'src/pages';
+
+// Lazy load EditorPage so that initial startup burden is reduced
+const EditorPage = React.lazy(() => import('src/pages/EditorPage'));
 
 export const Routes = () => {
   return (
     <div className="page">
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/contributors" component={ContributorsPage} />
-        <Route exact path="/faq-page" component={FAQPage} />
-        <Route exact path="/editor-page" component={EditorPage} />
-        <Route path="/" component={NotFoundPage} />
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/contributors" component={ContributorsPage} />
+          <Route exact path="/faq-page" component={FAQPage} />
+          <Route exact path="/editor-page" component={EditorPage} />
+          <Route path="/" component={NotFoundPage} />
+        </Switch>
+      </Suspense>
     </div>
   );
 };
